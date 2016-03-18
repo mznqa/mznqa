39e6ea51e0d8de1b01cc7352dd7cb6793a604218
Timmiy <915941096@qq.com>
2016-03-18 19:35:26 +0800

    1.添加了#pragma execution_character_set("utf-8")

--- mznqa/Classes/data/realTimeData/Monster.cpp
+++ mznqa/Classes/data/realTimeData/Monster.cpp
@@ -1,3 +1,4 @@
+#pragma execution_character_set("utf-8")
 #include "data/realTimeData/Monster.h"
 
 Monster::Monster()
--- mznqa/Classes/data/realTimeData/Monster.h
+++ mznqa/Classes/data/realTimeData/Monster.h
@@ -1,3 +1,4 @@
+#pragma execution_character_set("utf-8")
 #ifndef MZNQA_CLASSES_DATA_REALTIMEDATA_MONSTER_H_
 #define MZNQA_CLASSES_DATA_REALTIMEDATA_MONSTER_H_
 
