bac8a9421aa4fcd10a719de0ee9bf9e554c1dafa
Yeshiyong <wp.yeshiyong@outlook.com>
2016-05-16 01:26:53 +0800

    修正上次提交

--- mznqa/proj.android/jni/Android.mk
+++ mznqa/proj.android/jni/Android.mk
@@ -12,7 +12,6 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
               ../../Classes/AppDelegate.cpp \
-              ../../Classes/HelloWorldScene.cpp \
               ../../Classes/helper/bridge/Bridge.cpp \
               ../../Classes/logic/gameObject/card/CardBase.cpp \
               ../../Classes/logic/gameObject/card/CardRoad.cpp \
